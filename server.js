b90c10096f5d99535bd1da7379d6e652e95b0af2
Aniket <aniketrandhave15@gmail.com>
2022-04-19 18:25:28 +0530

    make it better

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ import Connection from "./db/connection1.js";
 import configApp from "./config-app.js";
 
 const app = express()
-const port = 4000
+// const port = 4000
+const PORT = process.env.PORT || 8000
 
 
 // config-app
@@ -21,7 +22,7 @@ app.use(configApp)
 Connection()
 
 
-app.listen(port, () => {
-    console.log(`server successfully running on http://localhost:${port} port`)
+app.listen(PORT, () => {
+    console.log(`server successfully running on http://localhost:${PORT} port`)
 })
 
